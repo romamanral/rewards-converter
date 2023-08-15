6cbbb8748112b64f88503a192e61120682bc47b5
romamanral <100014347+romamanral@users.noreply.github.com>
2023-08-15 09:17:38 +0530

    Add files via upload

---
+++ RewardValue.java
@@ -0,0 +1,26 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035; // Convert cash to miles
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(double milesValue, boolean isMiles) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035; // Convert miles to cash
+    }
+
+    // Get the cash value of the RewardValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get how many miles the RewardValue is worth
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
